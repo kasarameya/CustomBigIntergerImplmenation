5d3bc16c05f9517ee4817dbf85d36abd213f2a91
maneshreyash <maneshreyashs@gmail.com>
2018-09-20 03:20:18 -0500

    Addition, Subtraction and CompareTo with respective utilities at the bottom.

--- src/Num.java
+++ src/Num.java
@@ -56,133 +56,90 @@ public class Num implements Comparable<Num> {
             arr[len - i - 1] = (long) (s.charAt(i) - '0');
         }*/
     }
-
     public static Num add(Num a, Num b) {
-
-        int i = 0, j = 0;
-        long sum = 0;
-        long carry = 0;
-        //String temp = "";
-        //Num result = new Num("0000");
-        long[] result = new long[Math.max(a.len, b.len) + 1];
-        int index = 0;
-
-        while (i < a.len && j < b.len) {
-            sum = a.arr[i] + b.arr[j] + carry;
-            result[index] = sum % base;
-            //result.arr[index]= sum % 10;
-            //temp += sum % base;
-            index++;
-            carry = sum / base;
-            i++;
-            j++;
-        }
-
-        while (i < a.len) {
-            sum = a.arr[i] + carry;
-            result[index] = sum % base;
-            //result.arr[index]= sum % 10;
-            //temp += sum % base;
-            index++;
-            carry = sum / base;
-            i++;
-        }
-
-        while (j < b.len) {
-            sum = b.arr[j] + carry;
-            result[index] = sum % base;
-            //result.arr[index]= sum % 10;
-            //temp += sum % base;
-            index++;
-            carry = sum / base;
-            j++;
-        }
-
-        if (carry > 0) {
-            //temp += carry;
-            result[index] = carry;
-            index++;
-        }
-
         Num answer = new Num();
-        answer.arr = result;
-        answer.len = index;
-        return answer;
-    }
-
-    public static Num subtract(Num a, Num b) {
-        int i = 0, j = 0;
-        long diff = 0;
-        //long borrow = 0;
-        //String temp = "";
-        //Num result = new Num("0000");
-        int m = 0;
-        Num answer = new Num();
-        long[] result = new long[Math.max(a.len, b.len)];
-        int index = 0;
         Num x = new Num();
         Num y = new Num();
-
-        if (b.isNegative && !a.isNegative) {
-            answer = add(a, b);
-        } else if (a.isNegative && !b.isNegative) {
-            answer = add(a, b);
-            answer.isNegative = true;
-        } else if (!a.isNegative && !b.isNegative) {
-
+        long[] result = new long[Math.max(a.len,b.len)+1];
+        if((a.isNegative && !b.isNegative) || (!a.isNegative && b.isNegative))
+        {
             if (a.len > b.len) {
                 x = a;
                 y = b;
+                if(a.isNegative) answer.isNegative = true;
+                else if(b.isNegative) answer.isNegative = false;
             } else if (a.len < b.len) {
                 x = b;
                 y = a;
-                answer.isNegative = true;
+                if(a.isNegative) answer.isNegative = false;
+                else if(b.isNegative) answer.isNegative = true;
             } else if (a.len == b.len) {
-                if (a.compareTo(b) == 1 || a.compareTo(b) == 0) {
+                if (a.compareMagnitude(b) == 1 || a.compareMagnitude(b) == 0) {
                     x = a;
                     y = b;
-                } else if (a.compareTo(b) == -1) {
+                    if(a.isNegative) answer.isNegative = true;
+                    else if(b.isNegative) answer.isNegative = false;
+                } else if (a.compareMagnitude(b) == -1) {
+                    x = b;
+                    y = a;
+                    if(a.isNegative) answer.isNegative = false;
+                    else if(b.isNegative) answer.isNegative = true;
+                }
+            }
+            answer.arr = subhelper(x, y, result);
+        }
+        if(a.isNegative && b.isNegative) {
+            answer.arr = addhelper(a, b, result);
+            answer.isNegative = true;
+        }
+        else if(!a.isNegative && !b.isNegative)
+            answer.arr = addhelper(a, b, result);
+        answer.len = result.length;
+        return answer;
+    }
+
+    public static Num subtract(Num a, Num b)
+    {
+        Num answer = new Num();
+        long[] result = new long[Math.max(a.len,b.len)+1];
+        Num x = new Num();
+        Num y = new Num();
+
+        if(b.isNegative && !a.isNegative)
+        {
+            answer.arr = addhelper(a, b, result);
+        }
+        else if(a.isNegative && !b.isNegative)
+        {
+            answer.arr = addhelper(a, b, result);
+            answer.isNegative = true;
+        }
+        else if(!a.isNegative && !b.isNegative)
+        {
+            if(a.len > b.len){
+                x = a;
+                y = b;
+            }
+            else if(a.len < b.len){
+                x = b;
+                y = a;
+                answer.isNegative = true;
+            }else if(a.len == b.len){
+                if (a.compareMagnitude(b) == 1 || a.compareMagnitude(b) == 0){
+                    x = a;
+                    y = b;
+                }else if(a.compareMagnitude(b) == -1){
                     x = b;
                     y = a;
                     answer.isNegative = true;
                 }
             }
-
+            answer.arr = subhelper(x, y, result);
+        }else if(a.isNegative && b.isNegative){
+            b.isNegative = false;
+            answer = add(a, b);
         }
-
-        while (i < x.len && j < y.len) {
-            if (x.arr[i] >= y.arr[j]) {
-                diff = x.arr[i] - y.arr[j];
-                result[index] = diff;
-            } else {
-                int k = 0;
-                diff = (base + x.arr[i]) - y.arr[j];
-                if (x.arr[i + 1] != 0)
-                    x.arr[i + 1] -= 1;
-                else {
-                    k = i + 1;
-                    while (x.arr[k] == 0) {
-                        x.arr[k] = base - 1;
-                        //m = k;
-                        k++;
-                    }
-                    x.arr[k] -= 1;
-                }
-
-                result[index] = diff;
-            }
-
-            index++;
-            i++;
-            j++;
-        }
-        while (i < x.len) {
-            result[index] = x.arr[i];
-            index++;
-            i++;
-        }
-        answer.arr = result;
-        answer.len = index;
+        answer.len = result.length;
         return answer;
     }
 
@@ -342,14 +299,14 @@ public class Num implements Comparable<Num> {
     }
 
     public static void main(String[] args) {
-        Num x = new Num(00010101000);
+        Num x = new Num(100);
         //x.printList();
-        //Num y = new Num(4);
+        Num y = new Num(40);
         //y.printList();
 
         //System.out.println(y.compareTo(x));
-        //Num z = Num.subtract(x,y);
-        //z.printList();
+        Num z = Num.subtract(x,y);
+        z.printList();
 
         //Num c = Num.by2(x);
         //c.printList();
@@ -383,59 +340,36 @@ public class Num implements Comparable<Num> {
     // compare "this" to "other": return +1 if this is greater, 0 if equal, -1 otherwise
     public int compareTo(Num other) {
 
-        if (!this.isNegative && !other.isNegative) {
-            if (this.len > other.len) {
+        if(!this.isNegative && !other.isNegative)
+        {
+            return this.compareMagnitude(other);
+        }
+        else if(this.isNegative && other.isNegative)
+        {
+            int flag = this.compareMagnitude(other);
+            //System.out.println(flag);
+            if(flag == -1) {
+                //  System.out.println("OKAY");
                 return 1;
-            } else if (this.len < other.len) {
-                return -1;
-            } else {
-                int i = this.len - 1, j = other.len - 1;
-                while (i >= 0 && j >= 0) {
-
-                    if (this.arr[i] > other.arr[j]) {
-                        return 1;
-                    } else if (this.arr[i] < other.arr[j]) {
-                        return -1;
-                    } else {
-                        if (i == 0 && j == 0) {
-                            return 0;
-                        }
-                        i--;
-                        j--;
-                    }
-                }
             }
-        } else if (this.isNegative && other.isNegative) {
-            if (this.len > other.len) {
+            else if(flag == 1)
                 return -1;
-            } else if (this.len < other.len) {
-                return 1;
-            } else {
-                int i = this.len - 1, j = other.len - 1;
-                while (i >= 0 && j >= 0) {
-
-                    if (this.arr[i] > other.arr[j]) {
-                        return -1;
-                    } else if (this.arr[i] < other.arr[j]) {
-                        return 1;
-                    } else {
-                        if (i == 0 && j == 0) {
-                            return 0;
-                        }
-                        i--;
-                        j--;
-                    }
-                }
-            }
-        } else if (!this.isNegative && other.isNegative) {
+            else
+                return 0;
+        }
+        else if(!this.isNegative && other.isNegative)
+        {
             return 1;
-        } else if (this.isNegative && !other.isNegative) {
+        }
+        else if(this.isNegative && !other.isNegative)
+        {
             return -1;
         }
 
 
         return 0;
     }
+
 
     // Output using the format "base: elements of list ..."
     // For example, if base=100, and the number stored corresponds to 10965,
@@ -468,4 +402,122 @@ public class Num implements Comparable<Num> {
 
         return null;
     }
+
+
+
+
+
+
+
+
+
+    //UTILITY FUNCTIONS
+    public static long[] addhelper(Num x, Num y, long result[]){
+        int i = 0,j = 0;
+        long sum = 0;
+        long carry = 0;
+        int index = 0;
+        while(i < x.len && j < y.len)
+        {
+            sum = x.arr[i] + y.arr[j] + carry;
+            result[index] = sum % base;
+            index++;
+            carry = sum / base;
+            i++;
+            j++;
+        }
+
+        while(i < x.len)
+        {
+            sum = x.arr[i] + carry;
+            result[index] = sum % base;
+            index++;
+            carry = sum / base;
+            i++;
+        }
+
+        while(j < y.len)
+        {
+            sum =  y.arr[j] + carry;
+            result[index] = sum % base;
+            index++;
+            carry = sum / base;
+            j++;
+        }
+
+        if(carry > 0)
+        {
+            result[index] = carry;
+            index++;
+        }
+        return result;
+    }
+
+    public static long[] subhelper(Num x, Num y, long result[]){
+        int i = 0,j = 0;
+        int index = 0;
+        long diff;
+        while (i < x.len && j < y.len)
+        {
+            if (x.arr[i] >= y.arr[j])
+            {
+                diff = x.arr[i] - y.arr[j];
+                result[index] = diff;
+            }
+            else
+            {   int k = 0;
+                diff = (base + x.arr[i]) - y.arr[j];
+                if(x.arr[i + 1] != 0) {
+                    x.arr[i + 1] -= 1;
+                }
+                else
+                {
+                    k = i + 1;
+                    while(x.arr[k] == 0) {
+                        x.arr[k] = base - 1;
+                        k++;
+                    } x.arr[k] -= 1;
+                }
+                result[index] = diff;
+            }
+            index++;
+            i++;
+            j++;
+        }
+        while(i < x.len)
+        {
+            result[index] = x.arr[i];
+            index++;
+            i++;
+        }
+        return result;
+    }
+
+    public int compareMagnitude(Num other) {
+        int flag = 0;
+        if (this.len > other.len) {
+            return 1;
+        } else if (this.len < other.len) {
+            return -1;
+        } else if (this.len == other.len) {
+            int i = 0, j = 0;
+            while (i <this.len && j <other.len) {
+
+                if (this.arr[i] > other.arr[j]) {
+                    flag = 1;
+                } else if (this.arr[i] < other.arr[j]) {
+                    flag = -1;
+                } else if(this.arr[i] == other.arr[j]){
+                    flag = 0;
+                }
+                i++;
+                j++;
+            }
+        }
+        return flag;
+    }
 }
+
+
+
+
